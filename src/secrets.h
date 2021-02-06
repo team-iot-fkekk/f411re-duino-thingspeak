43e036774f6d82b6f0b30da9fc17c4e4bb9c1b57
team-iot-fkekk <team.iot.fkekk@gmail.com>
2021-02-07 01:53:38 +0800

    initial commit

---
+++ src/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials
+// and connection details
+
+#define SECRET_SSID "abidev"   // replace MySSID with your WiFi network name
+#define SECRET_PASS "abcd1234" // replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 1226276                   // replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "7GFT14DHFIJCEIO1" // replace XYZ with your channel write API Key
